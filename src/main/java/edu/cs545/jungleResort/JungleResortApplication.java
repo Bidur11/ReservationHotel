f09195fca24531bc37b8d72f4512710e97b7b44c
Bidur11 <bidur585@gmail.com>
2018-03-07 14:56:16 -0600

    HOtelReservation

---
+++ src/main/java/edu/cs545/jungleResort/JungleResortApplication.java
@@ -0,0 +1,18 @@
+package edu.cs545.jungleresort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@ComponentScan(basePackages = "edu.cs545.jungleresort")
+public class JungleResortApplication {
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungleResortApplication.class, args);
+	}
+}
